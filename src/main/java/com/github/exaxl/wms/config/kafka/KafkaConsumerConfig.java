19190a68e22775afa4859b06efecb7bd8a14b9dc
EXAXL <edwardXalphonseXleorio@gmail.com>
2026-08-31 17:13:49 +0200

    Slight change ack mode setup method

--- src/main/java/com/github/exaxl/wms/config/kafka/KafkaConsumerConfig.java
+++ src/main/java/com/github/exaxl/wms/config/kafka/KafkaConsumerConfig.java
@@ -11,7 +11,6 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
@@ -49,7 +48,6 @@ public class KafkaConsumerConfig {
 		factory.setConsumerFactory(customerConsumerFactory);
 		factory.setConcurrency(1);
 		factory.setCommonErrorHandler(errorHandler);
-	    factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.RECORD);
 		return factory;
 	}
 }
